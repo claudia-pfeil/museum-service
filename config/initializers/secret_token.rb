7d95d56748235997ad82251536b8e48c2fc2966b
Alex Coles <alex@alexbcoles.com>
2012-10-20 13:34:38 +0200

    Initial commit of newly-generated Rails app
    * rails new museumdienst -d postgresql -T
    
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Museumdienst::Application.config.secret_token = 'b9d1c00f2a876c64240e537917fc72b00f065767ef7f31941a2a1973c913e09a5ec0b133a1c1ea9b57f2286331b25858660190bc495a27539cc2a9f5855ba205'
