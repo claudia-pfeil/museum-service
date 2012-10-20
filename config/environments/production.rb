cc926e6e8b4a8e4891ffc18fbd604b683ecc7cb0
Alex Coles <alex@alexbcoles.com>
2012-10-20 14:33:23 +0200

    Add and set-up Devise for authentication
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Museumdienst::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'museumsdienst-hamburg.de' }
 
   # Enable threaded mode
   # config.threadsafe!
